43ac6c3c40f025e1310230e4eee5076bba041362
Daniel Taylor <danielfranktaylor@gmail.com>
2016-03-25 13:16:12 -0500

    Edited currentLocation

--- section2-team11-project/src/ui/getCurrentLocation.java
+++ section2-team11-project/src/ui/getCurrentLocation.java
@@ -11,5 +11,27 @@ public class getCurrentLocation {
 	/*
 	 * Needs to return current city in format: "TX/San_Antonio" or
 	 * zipcode (possibly 2 separate functions)
+	 * http://api.wunderground.com/api/d1b960fa65c6eccc/conditions/q/TX/San_Antonio.json
 	 */
+	
+	private String stateI;
+	private String stateN;
+	private Int zip;
+	
+	public void setLocation(int zipC){
+		zip = zipC;
+	}
+	
+	public void setLocation(String stateIn, String stateNa){
+		stateI = stateIn;
+		stateN = stateNa;
+	}
+	public String getLocation(){
+		return "/" + zip;
+	}
+	
+	public String getLocation(){
+		return "/" + stateI + "/" + stateN;
+	}
+	}
 }
