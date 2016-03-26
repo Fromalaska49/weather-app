59426ff05de7f764c2e462ebe2dbd660f53b04d8
Guadalupe Barboza <gbarboza1231@gmail.com>
2016-03-26 15:07:29 -0500

    Trying to get old JRE

--- section2-team11-project/src/weather/weather.java
+++ section2-team11-project/src/weather/weather.java
@@ -2,7 +2,7 @@ package weather;
 
 public class weather {
 	public static void main(String[] args) {
-		System.out.println("Good luck to Team Rocket!!");
+		System.out.println("Good luck to Team Rocket!!!");
 		loadAPI thingy = new loadAPI();
 		String location = "TX/San_Antonio";
 		//String location = "78249";
