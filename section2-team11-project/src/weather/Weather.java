80fff35c00f64a37e17821cb1438e100f3cf7c7a
Fromalaska49 <fromalaska49@gmail.com>
2016-03-26 17:12:34 -0500

    Commented Temporarily Broken Code

--- section2-team11-project/src/weather/Weather.java
+++ section2-team11-project/src/weather/Weather.java
@@ -6,9 +6,9 @@ public class Weather {
 		LoadAPI thingy = new LoadAPI();
 		//String location = "TX/San_Antonio";
 		String location = "99508";
-		System.out.println(thingy.getWeatherJSON(location));
-		System.out.println(thingy.getWeatherXML(location));
-		ProcessData city = new ProcessData();
-		System.out.println(city.getCurrentCondition(location));
+		//System.out.println(thingy.getWeatherJSON(location));
+		//System.out.println(thingy.getWeatherXML(location));
+		//ProcessData city = new ProcessData();
+		//System.out.println(city.getCurrentCondition(location));
 	}
 }
