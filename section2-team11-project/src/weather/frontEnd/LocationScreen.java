2e901ec2621d299b560ceb7b33384b57f4b519b7
nishantg95 <nishant.groveris@gmail.com>
2016-03-26 17:05:01 -0500

    Added labels to frontEnd

--- section2-team11-project/src/weather/frontEnd/LocationScreen.java
+++ section2-team11-project/src/weather/frontEnd/LocationScreen.java
@@ -12,11 +12,26 @@ import javax.swing.*;
  */
 public class LocationScreen extends JFrame {
 	
-	private JLabel firstScreen;
-	
+		
 	public LocationScreen(){
 		this.setVisible(true);
-		this.setSize(1920, 1080);
+		this.setSize(920, 480);
+		JPanel north = new JPanel();
+		JPanel south = new JPanel();
+		JPanel west = new JPanel();
+		JPanel east = new JPanel();
+		add(north,BorderLayout.NORTH);
+		add(south,BorderLayout.SOUTH);
+		add(west,BorderLayout.WEST);
+		add(east,BorderLayout.EAST);
+		this.setTitle("Team Rocket's US Weather App");
+		JLabel title = new JLabel("US Weather App");
+		north.add(title);
+		
+		/*
+		Title title = new Title(" Weather App");
+		this.add(title);
+		*/
 	}
 
 }
