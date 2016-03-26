f39e91804b1b65b9e4b477a68d781d568c84df72
Guadalupe Barboza <gbarboza1231@gmail.com>
2016-03-26 15:24:45 -0500

    renamed Weather

--- section2-team11-project/src/weather/Weather.java
+++ section2-team11-project/src/weather/Weather.java
@@ -1,6 +1,6 @@
 package weather;
 
-public class weather {
+public class Weather {
 	public static void main(String[] args) {
 		System.out.println("Good luck to Team Rocket!!!");
 		loadAPI thingy = new loadAPI();
