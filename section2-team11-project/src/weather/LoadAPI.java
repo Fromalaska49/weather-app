fcbc7ef34707b913cf5f6ab96efb571ebaac9abe
Andrew Hutton <fromalaska49@gmail.com>
2016-03-27 09:39:29 -0500

    Checks for change in location when caching

--- section2-team11-project/src/weather/LoadAPI.java
+++ section2-team11-project/src/weather/LoadAPI.java
@@ -56,6 +56,40 @@ public class LoadAPI{
 		return updateTime;
 	}
 	
+	/*
+	 * Used to update the location whenever a new call to the API is made
+	 */
+	private boolean setLocation(String location){
+		Path file = Paths.get("Location.txt");
+		Charset charset = Charset.forName("UTF-8");
+		try (BufferedWriter writer = Files.newBufferedWriter(file, charset)) {
+		    writer.write(location, 0, location.length());
+		    return true;
+		} catch (IOException x) {
+		    System.err.format("IOException: %s%n", x);
+		}
+		return false;
+	}
+	
+	/*
+	 * Gets the location of the last API call
+	 */
+	private String getLocation(){
+		String location = "";
+		Path file = Paths.get("Location.txt");
+		try (InputStream in = Files.newInputStream(file);
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				location += line;
+			}
+		} catch (IOException x) {
+			System.err.println(x);
+			return "undefined";
+		}
+		return location;
+	}
+	
 	
 	/* 
 	 * getWeatherJSON fetches data from the Weather API
@@ -63,8 +97,9 @@ public class LoadAPI{
 	 * String value
 	 */
 	public boolean downloadJSON(String location){
-		if(Instant.now().getEpochSecond() - getUpdateTime() < 900){
+		if(Instant.now().getEpochSecond() - getUpdateTime() < 900 && location.equals(getLocation())){
 			//less than 15 minutes since last API request
+			//and same location
 			return true;
 		}
 		String targetURL = "http://api.wunderground.com/api/d1b960fa65c6eccc/conditions/q/" + location + ".json";
@@ -84,6 +119,7 @@ public class LoadAPI{
 		try (BufferedWriter writer = Files.newBufferedWriter(file, charset)) {
 		    writer.write(targetURLContents, 0, targetURLContents.length());
 		    setUpdateTime();
+		    setLocation(location);
 		    return true;
 		} catch (IOException x) {
 		    System.err.format("IOException: %s%n", x);
@@ -97,8 +133,9 @@ public class LoadAPI{
 	 * String value
 	 */
 	public boolean downloadXML(String location) {
-		if(Instant.now().getEpochSecond() - getUpdateTime() < 900){
+		if(Instant.now().getEpochSecond() - getUpdateTime() < 900 && location.equals(getLocation())){
 			//less than 15 minutes since last API request
+			//and same location
 			return true;
 		}
 		String targetURL = "http://api.wunderground.com/api/d1b960fa65c6eccc/conditions/q/" + location + ".xml";
@@ -118,6 +155,7 @@ public class LoadAPI{
 		try (BufferedWriter writer = Files.newBufferedWriter(file, charset)) {
 		    writer.write(targetURLContents, 0, targetURLContents.length());
 		    setUpdateTime();
+		    setLocation(location);
 		    return true;
 		} catch (IOException x) {
 		    System.err.format("IOException: %s%n", x);
