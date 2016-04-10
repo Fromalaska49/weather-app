aece607574b8f996c75894b4d39f648fbac7e756
Guadalupe Barboza <gbarboza1231@gmail.com>
2016-04-10 14:27:49 -0500

    MainApp changed

--- section2-team11-project/src/weather/MainApp.java
+++ section2-team11-project/src/weather/MainApp.java
@@ -5,28 +5,102 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     private Stage primaryStage;
-    private BorderPane rootLayout;
+    private BorderPane root;
+    private final int SIZE = 60;
 
     @Override
     public void start(Stage primaryStage) {
     	
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Weather App");
-        initRootLayout();
-        showLocationScreen();
+    		/*root = new BorderPane();
+
+        root.setTop(getTopLabel());
+        
+        Scene scene = new Scene(root, 350, 300);
+        primaryStage.setTitle("BorderPane");
+        primaryStage.setScene(scene);
+        primaryStage.show();*/
+    	
+        //this.primaryStage = primaryStage;
+        //this.primaryStage.setTitle("Weather App");
+        //initRootLayout();
+        //showLocationScreen();
+    	
+    	GridPane root = new GridPane();
+        root.setHgap(6);
+        root.setVgap(6);
+        root.setPadding(new Insets(5));
+        
+        ColumnConstraints cons1 = new ColumnConstraints();
+        cons1.setHgrow(Priority.NEVER);
+        root.getColumnConstraints().add(cons1);
+
+        ColumnConstraints cons2 = new ColumnConstraints();
+        cons2.setHgrow(Priority.ALWAYS);
+        
+        root.getColumnConstraints().addAll(cons1, cons2);
+        
+        RowConstraints rcons1 = new RowConstraints();
+        rcons1.setVgrow(Priority.NEVER);
+        
+        RowConstraints rcons2 = new RowConstraints();
+        rcons2.setVgrow(Priority.ALWAYS);  
+        
+        root.getRowConstraints().addAll(rcons1, rcons2);
+        
+        Label lbl = new Label("Name:");
+        TextField field = new TextField();
+        ListView view = new ListView();
+        Button okBtn = new Button("OK");
+        Button closeBtn = new Button("Close");
+
+        GridPane.setHalignment(okBtn, HPos.LEFT);
+
+        root.add(lbl, 0, 0);
+        root.add(field, 1, 0, 3, 1);
+        root.add(view, 0, 1, 4, 2);
+        root.add(okBtn, 2, 3);
+        root.add(closeBtn, 3, 3);
+        
+        Scene scene = new Scene(root, 280, 300);
+
+        primaryStage.setTitle("New folder");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+    
+    private Label getTopLabel() {
+
+        Label lbl = new Label("Weather App");
+        lbl.setPrefHeight(SIZE);
+        lbl.prefWidthProperty().bind(root.widthProperty());
+        lbl.setStyle("-fx-border-style: dotted; -fx-border-width: 0 0 1 0;-fx-border-color: gray; -fx-font-weight: bold;-fx-padding:30");        
+        
+        return lbl;
+    }
+
 
     /**
      * Initializes the root layout.
      */
+    /*
     public void initRootLayout() {
         try {
             // Load root layout from fxml file.
@@ -43,11 +117,12 @@ public class MainApp extends Application {
             e.printStackTrace();
         }
     }
-
+     */
     /**
      * Shows the person overview inside the root layout.
      * @param model 
      */
+    /*
     public void showLocationScreen() {
         try {
         	// Load person overview.
@@ -66,7 +141,8 @@ public class MainApp extends Application {
             e.printStackTrace();
         }
     }
-
+    */
+    
     /**
      * Returns the main stage.
      * @return
