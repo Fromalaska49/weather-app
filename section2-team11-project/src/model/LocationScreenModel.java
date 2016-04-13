8ca0dc7b0bb9831a634a37e34d78b83f25b937cf
Andrew Hutton <fromalaska49@gmail.com>
2016-04-13 16:20:13 -0500

    Fixed location handling for state/city format

--- section2-team11-project/src/model/LocationScreenModel.java
+++ section2-team11-project/src/model/LocationScreenModel.java
@@ -77,7 +77,7 @@ public class LocationScreenModel {
 			location += this.zipCode;
 		}
 		else if(this.state.length() > 0 && this.city.length() > 0){
-			location = this.state + "\\" + this.city;
+			location = this.state + "/" + this.city.replace(" ", "_");
 		}
 		else{
 			System.out.println("Error: no location found (in LocationScreenModel)");
