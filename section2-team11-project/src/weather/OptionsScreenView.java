e51fc17ea58d00f498abc266f6e1264d418ba4b3
Daniel Taylor <danielfranktaylor@gmail.com>
2016-04-14 19:30:58 -0500

    Added temperature buttons

--- section2-team11-project/src/weather/OptionsScreenView.java
+++ section2-team11-project/src/weather/OptionsScreenView.java
@@ -1,33 +1,76 @@
 package weather;
 
+import java.io.PrintWriter;
+
+import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
 
 public class OptionsScreenView {
+	
+	private static ChoiceBox<String> tempField;
+	private GridPane grid;
+	private BorderPane border;
+	private Label tempLabel;
+	
 	public OptionsScreenView(){	
 	}
     public void start(Stage primaryStage, Scene scene) {
     	Stage stagePrev = primaryStage;
     	Scene scenePrev = scene;
-    	primaryStage.setTitle("Options Window");
-    	final Popup optionsWindow = new Popup();
+    	primaryStage.setTitle("Options Window");    
+    	tempLabel = new Label("Temprature Unit: ");
+    	
+    	
     	Button backBtn = new Button("Back");
     	backBtn.setOnAction(ScreenController .getBackListener(stagePrev, scenePrev));
+    	tempField = new ChoiceBox(FXCollections.observableArrayList("F", "C"));
+    	//tempField = "F";
+    	//tempField(getTempSet());
+    	setTempSet();
+    	grid = new GridPane();
+    	grid.add(backBtn, 0, 0);
+    	grid.add(tempLabel, 0, 1);
+    	grid.add(tempField, 3, 1);
+    	grid.setAlignment(Pos.CENTER);
     	
-    	HBox layout = new HBox(10);
-    	layout.setLayoutX(500);
-    	layout.setLayoutY(500);
-    	layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 10;");
-    	layout.getChildren().addAll(backBtn);
-    	primaryStage.setScene(new Scene(layout));
-    	//primaryStage.show();
-    	optionsWindow.show(primaryStage);
+    	border = new BorderPane();
+    	//border.setTop(headerText);
+    	border.setCenter(grid);
+    	
+    	
+    	Scene Optscene = new Scene(border, 800, 700);
+    	
+    	
+    	primaryStage.setScene(Optscene);
+    	primaryStage.show();
     	
     	
     }
+    /*
+    public static void writeToConfig(){
+    	try{
+    	PrintWriter writer = new PrintWriter("the-file-name.txt");
+    	writer.close();
+    	}catch(EXCEPTION e){
+    		
+    	}
+    }
+    */
+    public static void setTempSet(){
+    	tempField.setValue("F");
+    }
+    public static String getTempSet(){
+		return tempField.getValue().toString();
+	}
 }
